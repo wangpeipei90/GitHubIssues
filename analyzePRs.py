ed4c982ae5466fce48663cca0a49534d7b41bf06
Peipei Wang <pwang7@ncsu.edu>
2019-06-24 00:36:50 -0400

    add code get dependencies get duplicates

--- analyzePRs.py
+++ analyzePRs.py
@@ -75,7 +75,84 @@ def getPRInfoPerLangPerRepo(url2info_file,repo2info_file,file_csv,dir="/home/pei
         dict_writer.writerows(res)
     
     pickle.dump(dict_url2info,open(dir+"dict_url2info","wb"))
-          
+
+def getDuplicatedPRs(url2info_file,dir="/home/peipei/GitHubIssues/Orgs/apache/"):
+    res=dict()
+    from github import Github
+    github=Github("870589d0ca53859c2e4e54f71b1bdce3af5e609f")
+    with open(dir+url2info_file,"rb") as pickle_file:
+        dict_url2info = pickle.load(pickle_file)
+    for pr_url, pr_info in dict_url2info.items():
+        repo_url=pr_info['repo_url']
+        if repo_url not in res:
+            res[repo_url]=dict()
+        id,user,title,merged=api.getPRInfo(github, pr_url)
+        if title not in res[repo_url]:
+            res[repo_url][title]=[(id,pr_url,merged)]
+        else:
+            res[repo_url][title].append((id,pr_url,merged))
+    
+    for repo_url, titleInfos in res.items():
+        count1,count2=0,0
+        for title, prInfos in titleInfos.items():
+            if len(prInfos)>1:
+                count1+=1
+                count2+=(len(prInfos)-1)
+                print("in repo {} {} pull requests have the same title, they are: ".format(repo_url,len(prInfos),title))
+                print(sorted(prInfos,key=lambda prInfo: prInfo[0]))
+                print("--------------------------------")
+        if count1>0:
+            print("in repo {} total same-titled pull requests: {} groups in {} prs".format(repo_url,count1,count2))
+
+
+re_version=re.compile("to\s+(version)?\s*v?\d+\.\d+\.\d+")
+def getDependencyPRs(url2info_file,dir="/home/peipei/GitHubIssues/Orgs/apache/"):
+    definite_userCnt={"pyup-bot":0,"greenkeeperio-bot":0,"renovate[bot]":0,"dependabot-preview[bot]":0}
+    definite_res=list()
+    indefinite_res=list()
+    indefinite_res2=list()
+
+    from github import Github
+    github=Github("870589d0ca53859c2e4e54f71b1bdce3af5e609f")
+    
+    #test the user login name
+#     print(api.getPRInfo(github, "https://github.com/mozilla/experiments-viewer/pull/381"))
+#     print(api.getPRInfo(github, "https://github.com/mozilla/payments-client/pull/89"))
+#     print(api.getPRInfo(github, "https://github.com/mozilla/foundation.mozilla.org/pull/1935"))
+#     print(api.getPRInfo(github, "https://github.com/mozilla/delivery-console/pull/124"))
+    
+    
+    
+    
+    with open(dir+url2info_file,"rb") as pickle_file:
+        dict_url2info = pickle.load(pickle_file)
+    print("total pr:{}".format(len(dict_url2info)))
+     
+    for pr_url, pr_info in dict_url2info.items():
+        repo_url=pr_info['repo_url']
+        id,user,title,merged=api.getPRInfo(github, pr_url)
+        if user in definite_userCnt:
+            definite_userCnt[user]+=1
+            definite_res.append((pr_url,user,title,merged))
+        elif any([keyword in title for keyword in ['dependency','package']]):
+            indefinite_res.append((pr_url,user,title,merged))
+        elif re_version.search(title) is not None:
+            indefinite_res2.append((pr_url,user,title,merged))
+    
+    if definite_res:
+        print("Definite dependency PRs")
+        print(definite_userCnt)
+        for pr_url,user,title,merged in definite_res:
+            print("{},'{}','{}'".format(pr_url,user,title,merged))
+    if indefinite_res:
+        print("Indefinite dependency PRs by keywords")
+        for pr_url,user,title,merged in indefinite_res:
+            print("{},'{}','{}'".format(pr_url,user,title,merged))
+    if indefinite_res2:
+        print("Indefinite dependency PRs by version")
+        for pr_url,user,title,merged in indefinite_res2:
+            print("{},'{}','{}'".format(pr_url,user,title,merged))
+             
 def getPRLinkedIssueInOrg(url2info_file,file_csv,dir="/home/peipei/GitHubIssues/Orgs/apache/"):
     
     with open(dir+url2info_file,"rb") as pickle_file:
@@ -101,5 +178,15 @@ def getPRLinkedIssueInOrg(url2info_file,file_csv,dir="/home/peipei/GitHubIssues/
 #             dict_writer.writerows(infos)    
 if __name__ == '__main__':
 #     getUniquePRsInOrgs()
-    getPRInfoPerLangPerRepo("dict_url2info","dict_repo2info","repo_lang_file.csv","/home/peipei/GitHubIssues/Orgs/facebook/")
+#     getPRInfoPerLangPerRepo("dict_url2info","dict_repo2info","repo_lang_file.csv","/home/peipei/GitHubIssues/Orgs/facebook/")
+#     getDuplicatedPRs("dict_url2info","/home/peipei/GitHubIssues/Orgs/google/")
+#     getDuplicatedPRs("dict_url2info","/home/peipei/GitHubIssues/Orgs/facebook/")
+#     getDuplicatedPRs("dict_url2info","/home/peipei/GitHubIssues/Orgs/mozilla")
+#     getDuplicatedPRs("dict_url2info","/home/peipei/GitHubIssues/Orgs/apache/")
+    
+    
+#     getDependencyPRs("dict_url2info","/home/peipei/GitHubIssues/Orgs/google/")
+    getDependencyPRs("dict_url2info","/home/peipei/GitHubIssues/Orgs/facebook/")
+#     getDependencyPRs("dict_url2info","/home/peipei/GitHubIssues/Orgs/mozilla")
+#     getDependencyPRs("dict_url2info","/home/peipei/GitHubIssues/Orgs/apache/")
     pass
